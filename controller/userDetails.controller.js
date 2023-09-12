13e69c074510cc9e19a827e1f210a3ef28f37f85
DevTinaTi <tinadesk8@gmail.com>
2023-09-12 22:08:10 +0100

    add git_file and git_folder value

--- controller/userDetails.controller.js
+++ controller/userDetails.controller.js
@@ -1,18 +1,19 @@
 const { join } = require("path");
 
 const userDetails = (req, res) => {
-  function getCurrentUTCTime() {
+  function getUtcTimeString() {
     const now = new Date();
-    const minutes = now.getUTCMinutes();
-    const adjustedMinutes = Math.floor(Math.random() * 5) - 2; // Generate a random +/-2 minute adjustment
-    now.setUTCMinutes(minutes + adjustedMinutes);
-
-    const formattedTime = now.toISOString();
-
-    return formattedTime;
+    const year = now.getUTCFullYear();
+    const month = String(now.getUTCMonth() + 1).padStart(2, '0'); // Month is zero-based, so add 1 and pad with '0' if necessary
+    const day = String(now.getUTCDate()).padStart(2, '0');
+    const hours = String(now.getUTCHours()).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
+  
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
   }
 
-  const currentUTCTime = getCurrentUTCTime();
+  const utcTimeString = getUtcTimeString();
   console.log("Current UTC Time:", currentUTCTime);
     let day = new Date().getDay();
     const daysOfWeek = [
@@ -27,7 +28,7 @@ const userDetails = (req, res) => {
     return res.status(200).json({
       slack_name: `${req.query.slack_name}`,
       current_day: `${daysOfWeek[day]}`,
-      utc_time: `${currentUTCTime}`,
+      utc_time: `${utcTimeString}`,
       track: `${req.query.track}`,
       github_file_url: "https://github.com/NICKY-TECH/stageOne_HNG/blob/master/server.js",
       github_repo_url: "https://github.com/NICKY-TECH/stageOne_HNG",
